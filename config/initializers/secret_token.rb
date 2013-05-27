01060227e7f4b6041f689eea19803c7faf89cec0
belerweb <belerweb@gmail.com>
2013-05-27 23:27:52 +0800

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeyimiCom::Application.config.secret_token = '68ad39bbd903a9cc61d54995370fc6a674241cebfa638adeafb2cf28eb7b4d1c35a657f3a3158ad9a3abb057aaecc7daba16b1da1f3095ce05241bb3d31f6d17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeyimiCom::Application.config.session_store :cookie_store, key: '_deyimi.com_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeyimiCom::Application.config.session_store :active_record_store
